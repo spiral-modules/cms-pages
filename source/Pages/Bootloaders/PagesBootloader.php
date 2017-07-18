6b0a9eecfb96714a8ad39cd3aecf1cc912c5ea16
Valentin V <vvval@users.noreply.github.com>
2017-07-18 16:43:50 +0300

    /action/id required

--- source/Pages/Bootloaders/PagesBootloader.php
+++ source/Pages/Bootloaders/PagesBootloader.php
@@ -23,7 +23,7 @@ class PagesBootloader extends Bootloader
     {
         $route = new Route(
             'api_pages',
-            'api/pages[/<action>]',
+            'api/pages/<action>/<id>',
             'Spiral\Pages\Controllers\ApiController::<action>'
         );
 
